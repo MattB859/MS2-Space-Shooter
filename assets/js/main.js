fb7d620e127560eab2fea04a7969fa2373f5216f
MattB859 <mateus756@hotmail.com>
2021-08-11 14:33:14 +0000

    Add styling to canvas and background cover

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let img = document.getElementById("space-bg");
+
+/*ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, 500, 500);
+ctx.stroke();*/
