53d421f5bd329bddf63cd3d83e49f52cebbb41cf
MattB859 <mateus756@hotmail.com>
2021-08-12 14:28:21 +0000

    Bug: created for loop to iterate though the alien ufo so they can move down, code not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,41 +2,60 @@ let spaceship = document.getElementById("spaceship");
 let gameWindow = document.getElementById("gameWindow");
 
 //arrow function
-window.addEventListener("keydown", (e) => {
+window.addEventListener("keydown", function gameStart(e) {
 
     let left = parseInt(window.getComputedStyle(spaceship).getPropertyValue("left"));
-    
-    if (e.key === "ArrowLeft" && left > 0){
+
+    if (e.key === "ArrowLeft" && left > 0) {
         spaceship.style.left = left - 10 + "px";
 
-    } else if (e.key === "ArrowRight" && left <= 590){
-        spaceship.style.left = left + 10 + "px";  
-    } 
+    } else if (e.key === "ArrowRight" && left <= 590) {
+        spaceship.style.left = left + 10 + "px";
+    }
 })
 window.addEventListener("keyup", (e) => {
 
-    let up = parseInt(window.getComputedStyle(spaceship).getPropertyValue("up"));
+    let up = parseInt(window.getComputedStyle(spaceship).getPropertyValue("top"));
 
     if (e.key == "ArrowUp" && up > 0) {
         spaceship.style.up = up + 50 + "px";
 
-}  else if (e.key == "ArrowUP" && up <= 890){
-    spaceship.style.up = up - 10 + "px";
+    } else if (e.key == "ArrowUP" && up <= 890) {
+        spaceship.style.up = up - 10 + "px";
 
-} 
-    
+    }
+
 });
 
-let generaterocks = setInterval(()=> {
-   
-    let rock = document.createElement("div");
-    rock.classList.add("rocks");
+let generatealiens = setInterval(() => {
 
-   // This will place the ufo's in random postions for the attack..
-   let rockleft = parseInt(
-    window.getComputedStyle(rocks).getPropertyValue("left")
-   );
-   rocks.style.left = Math.floor(Math.random() * 590) + "px";
+    let alien = document.createElement("div");
+    alien.classList.add("aliens");
 
-   canvas.appendChild(rocks);
+    // This will place the ufo's in random postions for the attack..
+    let alienleft = parseInt(
+        window.getComputedStyle(alien).getPropertyValue("left")
+    );
+    alien.style.left = Math.floor(Math.random() * 590) + "px";
+
+    gameWindow.appendChild(alien);
 }, 1500);
+
+let movealiens = setInterval(() => {
+
+    let aliens = document.getElementsByClassName("aliens");
+
+    if (aliens != undefined) {
+        for (let i = 0; i < aliens.length; i++) {
+            /* This will increase the top of each alien ufo, so that t
+            he ufos can move downwards*/
+            
+            let alien = aliens[1];
+            let alientop = parseInt(
+                window.getComputedStyle(alien).getPropertyValue("top")
+            );
+
+            alien.style.top = alientop + 20 + "px";
+        }
+    }
+}, 450);
