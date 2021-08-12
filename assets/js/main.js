b650569e4f0e8f5e5e37a37c83ead7d1642eb69f
MattB859 <mateus756@hotmail.com>
2021-08-12 12:29:45 +0000

    Add a method to generate a value to let the ufo move in random positions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,42 @@
 let spaceship = document.getElementById("spaceship");
-let canvas = document.getElementById("canvas");
+let gameWindow = document.getElementById("gameWindow");
 
 //arrow function
 window.addEventListener("keydown", (e) => {
 
     let left = parseInt(window.getComputedStyle(spaceship).getPropertyValue("left"));
-    let up = parseInt(window.getComputedStyle(spaceship).getPropertyValue("up"));
-
+    
     if (e.key === "ArrowLeft" && left > 0){
         spaceship.style.left = left - 10 + "px";
 
     } else if (e.key === "ArrowRight" && left <= 590){
-        spaceship.style.left = left + 10 + "px";
-    
-    } else if (e.key === "ArrowUp" && up <= 0) {
-            spaceship.style.up = up - 10 + "px";
+        spaceship.style.left = left + 10 + "px";  
+    } 
+})
+window.addEventListener("keyup", (e) => {
 
-    }
+    let up = parseInt(window.getComputedStyle(spaceship).getPropertyValue("up"));
+
+    if (e.key == "ArrowUp" && up > 0) {
+        spaceship.style.up = up + 50 + "px";
+
+}  else if (e.key == "ArrowUP" && up <= 890){
+    spaceship.style.up = up - 10 + "px";
+
+} 
+    
 });
 
-let multiUfo = setInterval(()=>{
+let generaterocks = setInterval(()=> {
    
-    let ufo = document.createElement("div");
-    ufo.classList.add("ufo");
-   // This will place the ufo's in random postions for the attack
+    let rock = document.createElement("div");
+    rock.classList.add("rocks");
 
-   let ufoLeft = parseInt(window.getComputedStyle(ufo).getPropertyValue("left")
+   // This will place the ufo's in random postions for the attack..
+   let rockleft = parseInt(
+    window.getComputedStyle(rocks).getPropertyValue("left")
    );
-   ufo.style.left = Math.floor(Math.random() * 450) + "px";
+   rocks.style.left = Math.floor(Math.random() * 590) + "px";
 
-   canvas.appendChild(rock);
+   canvas.appendChild(rocks);
 }, 1500);
